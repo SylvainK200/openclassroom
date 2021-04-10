ffed8b98a15fe5d8dfd0c3bc65b3e2a82e948417
SylvainK200 <kouemosylvain@gmail.com>
2021-04-10 05:58:11 +0100

    passer des arguments a une vue

--- store/urls.py
+++ store/urls.py
@@ -5,4 +5,6 @@ from . import views # import views so we can use them in urls.
 
 urlpatterns = [
     url(r'^$', views.listing), # "/store" will call the method "index" in "views.py"
+    url (r'^(?P<album_id>[0-9]+)/$',views.detail),
+    url (r'^search/$',views.search)
 ]
